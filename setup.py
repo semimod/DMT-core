7b2c1eb8d20d49a860aaf2b9175555b49bb62db6
Mario Krattenmacher <mario.krattenmacher@web.de>
2022-02-21 14:40:17 +0100

    restructuring the gitlab build chain to reduce runtime

--- setup.py
+++ setup.py
@@ -12,7 +12,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/dmt-development/dmt",
-    # packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/dmt-development/dmt",
+        "Documentation": "https://dmt-development.gitlab.io/dmt-core/",
+        "Source Code": "https://gitlab.com/dmt-development/dmt",
+    },
     packages=setuptools.find_namespace_packages(include=["DMT.*"]),
     license="GNU GPLv3+",
     classifiers=["Programming Language :: Python :: 3.8", "Operating System :: OS Independent"],
