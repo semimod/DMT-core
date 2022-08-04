1ff1f1e7f86a91f868a710cad3b653a6e5c4dda0
Mario <mario.krattenmacher@semimod.de>
2022-08-04 09:08:31 +0200

    sorted the devices for "mismatch" and normal

--- DMT/core/dut_lib.py
+++ DMT/core/dut_lib.py
@@ -1186,7 +1186,13 @@ class DutLib(object):
             with doc.create(Subsection("Geometry Overview")):
                 dut_types = list(set([dut.dut_type for dut in self]))
                 for dut_type in dut_types:
-                    flavors = [dut.flavor for dut in self if dut.dut_type == dut_type]
+                    try:
+                        flavors = [dut.flavor for dut in self if dut.dut_type == dut_type]
+                    except AttributeError:
+                        for dut in self:
+                            if not hasattr(dut, "flavor"):
+                                dut.flavor = None
+                        flavors = [dut.flavor for dut in self if dut.dut_type == dut_type]
                     flavors = list(set(flavors))  # cast to unique
 
                     for flavor in flavors:
